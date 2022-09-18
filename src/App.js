6163dc728e1fc41bbca86e8b1edc5801a99fbf52
Tatonq <35265855+Tatonq@users.noreply.github.com>
2022-09-18 21:37:46 +0700

    Update App.js
    Add Liff

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react';
+import LiffComponents from './Components/LiffComponent'
 function App(props) {
   const { params } = props
   const navigate = useNavigate()
@@ -20,7 +21,7 @@ function App(props) {
     <div className="App">
       <h1>Hello, ยินดีต้อนรับนะ :D</h1>
       <h1>{params}</h1>
-      {/* <LiffComponents/> */}
+      <LiffComponents/>
     </div>
   );  
 }
