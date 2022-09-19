963d154344460420a3dfaec210f1292c4425d775
Tatonq <tonloveyou7@gmail.com>
2022-09-19 19:46:18 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useNavigate, useLocation } from 'react-router-dom'
 import { useEffect } from 'react';
 import LiffComponents from './Components/LiffComponent'
-// import Navigation from './Components/Navigation'
+import Navigation from './Components/Navigation'
 // import DoorComponent from './Components/DoorComponent';
 // import { ThreeDots } from 'react-loader-spinner'
 function App() {
@@ -11,7 +11,7 @@ function App() {
   const navigator = useNavigate()
   const queryParams = new URLSearchParams(search).get('page')
   // const { params } = props
-
+  console.log(queryParams);
   useEffect(() => {
     // liffInit();
     if(queryParams === 'register') {
@@ -27,10 +27,9 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, ยินดีต้อนรับนะ :D</h1>
-      <h1>{'ชื่อจาก parameter'+ queryParams}</h1>
+      <h1>{'ชื่อจาก parameter '+ queryParams}</h1>
       <h2>h2 นะ</h2>
-
-      <LiffComponents/>
+      {/* <LiffComponents/> */}
       {/* {params === 'controller99-1' ? <ThreeDots/> : <DoorComponent/>} */}
     </div>
   );  
