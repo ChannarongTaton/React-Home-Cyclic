473f11e935a2f645a9039978cafc52832356cdbc
Tatonq <tonloveyou7@gmail.com>
2022-09-19 20:47:18 +0700

    Edit params App and Routes

--- src/App.js
+++ src/App.js
@@ -4,44 +4,50 @@ import { useNavigate, useLocation } from 'react-router-dom'
 import { useEffect } from 'react';
 import LiffComponents from './Components/LiffComponent'
 import DoorComponent from './Components/DoorComponent';
-import axios from 'axios';
 // import { ThreeDots } from 'react-loader-spinner'
-function App() {
+function App(props) {
+  const { params } = props
   let search = useLocation().search;
   const navigator = useNavigate()
-  const queryParams = new URLSearchParams(search).get('liff.state')
-  // const { params } = props
-  // console.log(queryParams);
-  const fetchData=()=> {
-    axios.get(`${process.env.REACT_APP_API}/Items`)
-    .then(response => {
-      
-    })
-    .catch(err => console.log(err))
-    
-  }
-  useEffect(() => {
-    // liffInit();
-    fetchData()
-    if(queryParams === '?page=register') {
-      navigator('/register')
-    } else if (queryParams === '?page=controller') {
-      return <LiffComponents/>
-    } else if (queryParams === '?page=controller99-1'){
-      return <DoorComponent/>
-    }
-    // eslint-disable-next-line
-  },[])
 
-  return (
-    <div className="App">
-      <h1>Hello, ยินดีต้อนรับนะ :D</h1>
-      <h1>{'ชื่อจาก parameter '+ queryParams}</h1>
-      <h2>h2 นะ</h2>
-      {/* <LiffComponents/> */}
-      {/* {params === 'controller99-1' ? <ThreeDots/> : <DoorComponent/>} */}
-    </div>
-  );  
+  if(params === '?page=register') {
+    navigator('/register')
+  } else if (params === '?page=controller') {
+    return (
+      <div className="App">
+               <h1>Hello, ยินดีต้อนรับนะ :D</h1>
+       <h1>{'props '+ params}</h1>
+  <h1>{'search '+ search}</h1>
+        <LiffComponents/>
+      </div>
+    )
+  } else if (params === '?page=controller99-1'){
+    return <DoorComponent/>
+  }
+
+
+  // useEffect(() => {
+  //   // liffInit();
+  //   if(params === '?page=register') {
+  //     navigator('/register')
+  //   } else if (params === '?page=controller') {
+  //     return <LiffComponents/>
+  //   } else if (params === '?page=controller99-1'){
+  //     return <DoorComponent/>
+  //   }
+  //   // eslint-disable-next-line
+  // },[])
+
+  // return (
+  //   <div className="App">
+  //     <h1>Hello, ยินดีต้อนรับนะ :D</h1>
+  //     <h1>{'props '+ params}</h1>
+  //     <h1>{'search '+ search}</h1>
+  //     <h2>h2 นะ</h2>
+  //     {/* <LiffComponents/> */}
+  //     {/* {params === 'controller99-1' ? <ThreeDots/> : <DoorComponent/>} */}
+  //   </div>
+  // );  
 }
 
 export default App;
--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-// import { useLocation } from 'react-router-dom'
 import App from './App'
 import RegisterComponents from './Components/RegisterComponents'
 import LiffComponent from './Components/LiffComponent'
 import DoorComponent from './Components/DoorComponent'
 const MyRoute=()=> {
-        // let search = useLocation().search;
-        // const queryParams = new URLSearchParams(search).get('page')
+        const queryParams = new URLSearchParams(window.location.search);
+        const params = queryParams.get('liff.state');
     return(
         <Router>
             <Routes>
-                <Route path='/' element={<App/>}/>
+                <Route path='/' element={<App params={params}/>}/>
                 <Route path="/register" element={<RegisterComponents/>}/>
                 <Route path="/liff-home" element={<LiffComponent/>}/>
                 <Route path="/home2" element={<DoorComponent/>}/>
