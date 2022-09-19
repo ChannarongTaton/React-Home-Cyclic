173c0f981dbffcd440fcbefd1db1cd26a03afd77
Tatonq <tonloveyou7@gmail.com>
2022-09-19 18:18:55 +0700

    Restart

--- src/App.js
+++ src/App.js
@@ -6,33 +6,34 @@ import { useEffect } from 'react';
 import LiffComponents from './Components/LiffComponent'
 // import Navigation from './Components/Navigation'
 import DoorComponent from './Components/DoorComponent';
-function App() {
+import { ThreeDots } from 'react-loader-spinner'
+function App(props) {
+  const { params } = props
   let search = useLocation.search;
   const navigator = useNavigate()
-  const nameId = new URLSearchParams(search).get('page')
+  const nameId = new URLSearchParams(search).get(params)
 
-  // if(params === 'register') {
-  //   navigate('/register')
-  // } else if (params === 'controller') {
-  //   navigate('/liff-home')
-  // } else if (params === 'controller99-1'){
-  //   navigate('/home2')
-  // }
+  
   useEffect(() => {
     // liffInit();
     if(nameId === 'register') {
+      console.log(nameId);
       navigator('/register')
     } else if (nameId === 'controller') {
       navigator('/liff-home')
+      console.log(nameId);
     } else if (nameId === 'controller99-1'){
       navigator('/home2')
+      console.log(nameId);
     }
     // eslint-disable-next-line
   },[])
   return (
     <div className="App">
       <h1>Hello, ยินดีต้อนรับนะ :D</h1>
+      <h1>{params}</h1>
       <h1>{nameId}</h1>
+      <ThreeDots/>
       {/* {params === 'controller99-1' ? <DoorComponent/> : <LiffComponents/>} */}
       {/* <LiffComponents/> */}
       {/* <Navigation/> */}
