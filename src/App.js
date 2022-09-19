087d00f579bbfbe6292a56845011d6e8c392f8f6
Tatonq <tonloveyou7@gmail.com>
2022-09-19 15:15:06 +0700

    Update App.js
    แก้การเปลี่ยนหน้าที่รับค่ามาจาก params

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react';
-import LiffComponents from './Components/LiffComponent'
-import Navigation from './Components/Navigation'
+// import LiffComponents from './Components/LiffComponent'
+// import Navigation from './Components/Navigation'
 import DoorComponent from './Components/DoorComponent';
 function App(props) {
   const { params } = props
   const navigate = useNavigate()
-  if(params === 'register') {
-    navigate('/register')
-  } else if (params === 'controller') {
-    navigate('/liff-home')
-  } else if (params === 'controller99-1'){
-    navigate('/home2')
-  }
-  // useEffect(() => {
-  //   if(params === 'register') {
-  //     navigate('/register')
-  //   } else if (params === 'controller') {
-  //     navigate('/liff-home')
-  //   } else if (params === 'controller99-1'){
-  //     navigate('/home2')
-  //   }
-  //   // eslint-disable-next-line
-  // },[])
+  // if(params === 'register') {
+  //   navigate('/register')
+  // } else if (params === 'controller') {
+  //   navigate('/liff-home')
+  // } else if (params === 'controller99-1'){
+  //   navigate('/home2')
+  // }
+  useEffect(() => {
+    if(params === 'register') {
+      navigate('/register')
+    } else if (params === 'controller') {
+      navigate('/liff-home')
+    } else if (params === 'controller99-1'){
+      navigate('/home2')
+    }
+    // eslint-disable-next-line
+  },[])
   return (
     <div className="App">
       <h1>Hello, ยินดีต้อนรับนะ :D</h1>
