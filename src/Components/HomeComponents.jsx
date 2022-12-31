b0be9734fe60ff3ec678f9642a8975f41c89cb8f
Tatonq <tonloveyou7@gmail.com>
2022-12-31 17:30:55 +0700

    Update CSS and loading screen
    For 99

--- src/Components/HomeComponents.jsx
+++ src/Components/HomeComponents.jsx
@@ -2,7 +2,7 @@ import style from '../css/Home.module.css'
 import {BsLightbulb, BsLightbulbOffFill} from 'react-icons/bs'
 import { useState, useEffect }from 'react'
 import axios from 'axios'
-import { RotatingLines, Blocks } from 'react-loader-spinner'
+import { RotatingLines, Blocks, ProgressBar } from 'react-loader-spinner'
 function HomeComponents(props) {
 
 
@@ -11,9 +11,11 @@ function HomeComponents(props) {
   const [statein, setStateIn] = useState([])  //ดึงข้อมูลจาก api อุปกรณ์มาเก็บไว้เป็น list 0 1 2
   const [loading, setLoading] = useState(false) // Icon loading เมื่อกดจะให้แสดง แล้วเมื่อโหลดเสร็จให้ปิด
   const fetchData=()=> {
+    setLoading(true)
     axios.get(`${process.env.REACT_APP_API}/Items`)
     .then(response => {
       setStateIn(response.data)
+      setLoading(false)
     })
     .catch(err => console.log(err))
   }
@@ -39,28 +41,36 @@ function HomeComponents(props) {
   },[])
 
   return(
-      <div className={style.HomeDiv}>
-        <div>
-        {loading === false ? statein.map((home, index) => (
-          <div>
-            <div key={index} className={style.HomeCom}>
-              <h1>{home.name}</h1>
-              <div onClick={()=>trickBtn(home.id, home.isActive, lineName, home.name)} className={style.button_css}>
-              {loading === false ? home.id == 3 ? "สั่งประตูทำงาน" : (home.isActive === true ? <BsLightbulb className={style.Icons}/> : <BsLightbulbOffFill className={style.Icons}/>) : <RotatingLines/>}
+    <>
+      {loading === false ? statein.map((home, index) => (
+        <div className={style.HomeDiv}>
+              <div key={index} className={style.HomeCom}>
+                <h1>{home.name}</h1>
+                <div onClick={()=>trickBtn(home.id, home.isActive, lineName, home.name)} className={style.button_css}>
+                {loading === false ? home.id == 3 ? "สั่งประตูทำงาน" : (home.isActive === true ? <BsLightbulb className={style.Icons}/> : <BsLightbulbOffFill className={style.Icons}/>) : <RotatingLines/>}
+                </div>
+                <h5>{home.id == 3 ? "" : home.isActive === true ? 'สถานะ : ปิด' : 'สถานะ : เปิด'}</h5>
+                <h5>{"สั่งทำงานโดย : " + home.userActive}</h5>
               </div>
-              <h5>{home.id == 3 ? "" : home.isActive === true ? 'สถานะ : ปิด' : 'สถานะ : เปิด'}</h5>
-              <h5>{"สั่งทำงานโดย : " + home.userActive}</h5>
-            </div>
-          </div>
+            
+        </div>
         )) : 
-        <div className={style.Circles}>
-          <Blocks visible={true} height="150" width="150"
-          ariaLabel="blocks-loading" wrapperStyle={{}}
-          wrapperClass="blocks-wrapper"/>
-        </div>
-        }
-        </div>
+        <div className="container d-flex justify-content-center mt-5">
+        <div style={{"padding-top": '60%'}}>
+          <h1>กำลังโหลด..</h1>&nbsp;
+          <ProgressBar
+          height="150"
+          width="150"
+          ariaLabel="progress-bar-loading"
+          wrapperStyle={{}}
+          wrapperClass="progress-bar-wrapper"
+          borderColor = '#000000'
+          barColor = '#3385ff'
+          />
+          </div>
       </div>
+      }
+      </>
   )
 }
 
